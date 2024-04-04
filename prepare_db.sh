1c3b2971e9fce1034003c85c59e9f115967232f3
jaycode <teguhwpurwanto@gmail.com>
2024-04-04 13:48:45 +0000

    Complete the project

--- prepare_db.sh
+++ prepare_db.sh
@@ -1,3 +1,3 @@
-PGPASSWORD="$DB_PASSWORD" psql --host 127.0.0.1 -U myuser -d mydatabase -p 5433 < /workspace/project/db/1_create_tables.sql
-PGPASSWORD="$DB_PASSWORD" psql --host 127.0.0.1 -U myuser -d mydatabase -p 5433 < /workspace/project/db/2_seed_users.sql
-PGPASSWORD="$DB_PASSWORD" psql --host 127.0.0.1 -U myuser -d mydatabase -p 5433 < /workspace/project/db/3_seed_tokens.sql
+PGPASSWORD="$DB_PASSWORD" psql --host 127.0.0.1 -U myuser -d mydatabase -p 5433 < ./db/1_create_tables.sql
+PGPASSWORD="$DB_PASSWORD" psql --host 127.0.0.1 -U myuser -d mydatabase -p 5433 < ./db/2_seed_users.sql
+PGPASSWORD="$DB_PASSWORD" psql --host 127.0.0.1 -U myuser -d mydatabase -p 5433 < ./db/3_seed_tokens.sql
